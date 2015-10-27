68540c0ca22d6c4c962df7f0b42073e53c8547a0
Brunno dos Santos <squiter85@gmail.com>
2015-10-27 10:05:03 -0200

    Add the Place NSManagedObjects

---
+++ CoreData/Supporting Files/Place.h
@@ -0,0 +1,22 @@
+//
+//  Place.h
+//  CoreData
+//
+//  Created by Treinamento Mobile on 10/27/15.
+//  Copyright © 2015 Treinamento Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Place : NSManagedObject
+
+// Insert code here to declare functionality of your managed object subclass
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#import "Place+CoreDataProperties.h"
---
+++ CoreData/Supporting Files/Place.m
@@ -0,0 +1,15 @@
+//
+//  Place.m
+//  CoreData
+//
+//  Created by Treinamento Mobile on 10/27/15.
+//  Copyright © 2015 Treinamento Mobile. All rights reserved.
+//
+
+#import "Place.h"
+
+@implementation Place
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
