79a7f17abea2ae11eb74cc778b752244632ac4ee
Brunno dos Santos <squiter85@gmail.com>
2015-10-27 10:27:35 -0200

    Examples for `save` and `fetch` methods

--- CoreData/Controllers/ViewController.h
+++ CoreData/Controllers/ViewController.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+#import "Place.h"
 
 @interface ViewController : UIViewController
 
--- CoreData/Controllers/ViewController.m
+++ CoreData/Controllers/ViewController.m
@@ -24,4 +24,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)newEntry {
+    NSManagedObjectContext *context = ((AppDelegate *) [UIApplication sharedApplication].delegate).managedObjectContext;
+    
+    Place *place = [NSEntityDescription insertNewObjectForEntityForName:@"Place" inManagedObjectContext:context];
+    
+    //place.name = @"Belo Horizonte";
+    //place.image = NSData de aumguma coisa
+    
+    NSError *error = nil;
+    if (![context save:&error]) {
+        NSLog(@"%@", error);
+    }
+}
+
+- (void)fetchEntries {
+    NSManagedObjectContext *context = ((AppDelegate *) [UIApplication sharedApplication].delegate).managedObjectContext;
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Place"];
+    request.predicate = [NSPredicate predicateWithFormat:@"name = %@", @"São Paulo"];
+    request.sortDescriptors = @[ [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES] ];
+    
+    NSArray *array = [context executeFetchRequest:request error:nil];
+    for (Place *p in array) {
+        NSLog(@"%@", p.name);
+    }
+}
 @end
